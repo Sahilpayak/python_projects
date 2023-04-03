c3851dfbc3f3f217e71fec443f2c90f630ebe5eb
Sahilpayak <129717738+Sahilpayak@users.noreply.github.com>
2023-04-03 11:22:53 +0530

    Add files via upload

---
+++ sahil3.py
@@ -0,0 +1,19 @@
+a=int(input("enter any number"))
+b=int(input("enter any number"))
+#singale line if
+if a>10:
+
+    print("A is greate than 10")
+#if...else
+if a>b:
+    print("a is greate than b")
+else:
+    print("b is greater than a")
+#ladder
+if a>b:
+    print("a is greate than b")
+elif b>a:
+    print("b is greate than a")
+else:
+    print("both are equal")
+          
