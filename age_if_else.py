14b0fafada4dd1745bcb8f8767bc86f1b765e359
Sahilpayak <129717738+Sahilpayak@users.noreply.github.com>
2023-04-05 00:17:27 +0530

    Add files via upload

---
+++ age_if_else.py
@@ -0,0 +1,12 @@
+a=int(input("Enter your age \n"))
+if a>=18 and a<=24:
+    print("You are eligble")
+else:
+    print("You are anelidble")
+b=int(input("preas 1 for marid,preas 2 for unmerid"))
+if b==1:
+    print("Your are marid")
+elif b==2:
+    print ("you are unmaid")
+else:
+    print ("wrong input")
---
+++ list.py
@@ -0,0 +1,12 @@
+'''n=[12,13,45]
+for num in n:
+    print(num,end="")'''
+
+
+#wap to print a new list containing the squares of the numbers
+#list 
+
+n=[12,22,32,52,63,55,67,52]
+
+print([i**2 for i in n if i%2 ==0])
+
